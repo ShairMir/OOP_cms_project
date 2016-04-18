1c6159e5a149c2e4ad67cb4891210c5d5ec9720c
Shair Mir <s.mir89@hotmail.com>
2016-04-18 23:12:32 +0200

    PDO connection added

--- includes/Database.class.php
+++ includes/Database.class.php
@@ -2,36 +2,69 @@
 
 require("config.php");
 
+
+
 class Database {
 
-    public $connection;
+	private $_dbHost = DB_HOST;
+	private $_dbUsername = DB_USER;
+	private $_dbPassword = DB_PASS;
+	private $_dbName = DB_NAME;
 
-    public function __construct($dbHost, $dbUsername, $dbPassword, $dbName) {
+	public function __construct() {
 
-        $this->connection = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+		try {
 
-        if ($this->connection->connect_errno) {
-            die("DB CONNECTION FAILED " . $this->connection->connect_error);
-        }
+			$handler = new PDO('mysql:host=' . $this->_dbHost . ';dbname=' . $this->_dbName, $this->_dbUsername, $this->_dbPassword);
 
-    }
+			$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    public function query($sql) {
+		} catch(PDOException $e) {
 
-    	return $result = $this->connection->query($sql);
+			die('FAILED DB CONNECTION' . $e->getMessage());
 
-    }
+		}
 
-    public function escape_string($string) {
-
-    	return $this->connection->real_escape_string($string);
-
-    }
-
-
+	}
 
 }
 
-$database = new Database($dbHost, $dbUsername, $dbPassword, $dbName);
+$database = new Database;
+
+
+
+
+
+// class Database {
+
+//     public $connection;
+
+//     public function __construct($dbHost, $dbUsername, $dbPassword, $dbName) {
+
+//         $this->connection = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName);
+
+//         if ($this->connection->connect_errno) {
+//             die("DB CONNECTION FAILED " . $this->connection->connect_error);
+//         }
+
+//     }
+
+//     public function query($sql) {
+
+//     	return $result = $this->connection->query($sql);
+
+//     }
+
+//     public function escape_string($string) {
+
+//     	return $this->connection->real_escape_string($string);
+
+//     }
+
+
+
+// }
+
+// $database = new Database($dbHost, $dbUsername, $dbPassword, $dbName);
 
 ?>
